0cc2019dac866a7db65a6295b111299e7c22b825
lucas orr <lucaskianorr@gmail.com>
2026-06-01 01:32:24 -0700

    Separate chord/dyad display, default maxSize 18, description edits
    Chords and intervals now tracked independently so playing a dyad
    doesn't erase the last chord. Default max N changed to 18.

--- mos-data.js
+++ mos-data.js
@@ -28,12 +28,12 @@ const TEMPERAMENTS = [
   { name: 'Superpyth', mos: ['5L_2s', '5L_7s'], genRange: [707.69, 713.08],
     commas: ['64/63', '245/243'], commaNames: ['Archytas comma', 'sensamagic comma'],
     wikiUrl: 'https://en.xen.wiki/w/Superpyth',
-    description: 'Sharp fifths. Two fifths = 8/7. Opposite of meantone. 22 and 27edo are quintissential superpyth tunings.' },
+    description: 'Sharp fifths. Two fifths = 8/7. Opposite of meantone. 22 and 27edo are quintissential superpyth tunings. Sounds snazzy - classic xen sound.' },
 
   { name: 'Ultrapyth', mos: ['5L_2s', '5L_7s'], genRange: [713.08, 720.0],
     commas: ['64/63'], commaNames: ['Archytas comma'],
     wikiUrl: 'https://en.xen.wiki/w/Archytas_clan#Ultrapyth',
-    description: 'Very sharp fifths near 5edo. Extends oceanfront (2.3.7.13/5). 5/4 mapped to +14 fifths. Melodically starts to collapse here; composition may be tricky.' },
+    description: 'Very sharp fifths near 5edo. Extends oceanfront (2.3.7.13/5). 5/4 mapped to +14 fifths. Melodically starts to collapse here; composition may be tricky. Some weird stuff can happen where the extremely small comma-step can give you a just 3/2 again, by correcting your very sharp superpyth fifth.' },
 
 //  { name: 'Schismatic', mos: ['5L_2s', '5L_7s', '12L_5s'], genRange: [700.0, 701.5],
 //    commas: ['32805/32768'], commaNames: ['schisma'],
@@ -49,7 +49,7 @@ const TEMPERAMENTS = [
   { name: 'Mabila', mos: ['2L_5s', '2L_7s', '9L_7s'], genRange: [666.0, 670.0],
     commas: ['268435456/263671875'], commaNames: ['mabila comma'],
     wikiUrl: 'https://en.xen.wiki/w/Mabila_family',
-    description: 'Flat fifths like mavila but three generators = 5/2. Ten bad fifths reach a good one. 9L 7s MOS has good triads.' },
+    description: 'Flat fifths like mavila but three generators = 5/2. Ten bad fifths reach a good one. 9L 7s MOS has good triads. <br> <br> Basically mavila but you can consider it even flatter, I guess?' },
 
   { name: 'Wilsec', mos: ['2L_5s', '2L_7s', '2L_9s'], genRange: [535.7, 540.0],
     commas: ['49/48'], commaNames: ['semaphoresma'],
@@ -75,17 +75,17 @@ const TEMPERAMENTS = [
   { name: 'Mohajira', mos: ['3L_4s', '7L_3s'], genRange: [342.86, 350.0],
     commas: ['81/80', '121/120', '243/242'], commaNames: ['syntonic comma', 'biyatisma', 'rastma'],
     wikiUrl: 'https://en.xen.wiki/w/Mohajira',
-    description: 'Neutral thirds temperament. <a href="https://en.xen.wiki/w/Dicot_family" target="_blank" style="color:#c49bd4;text-decoration:none">Dicot</a> tuning, e.g. this divides the fifth into 2 parts. Generator splits the fifth into two equal ~350¢ thirds.' },
+    description: 'Neutral thirds temperament. <a href="https://en.xen.wiki/w/Dicot_family" target="_blank" style="color:#c49bd4;text-decoration:none">Dicot</a> tuning, e.g. this divides the fifth into 2 parts. This is this as the "flat" portion of neutral third temperaments, which when stacked, gives you meantone (the flat fifth temperament). Contains the just 11/9 neutral third, which is the lowest complexity neutral interval, which gives it extra sauce.' },
 
   { name: 'Hemififths', mos: ['3L_4s', '7L_3s', '10L_7s'], genRange: [350.0, 352.94],
     commas: ['5120/5103', '10976/10935'], commaNames: ['hemifamity comma', 'hemimage comma'],
     wikiUrl: 'https://en.xen.wiki/w/Hemififths',
-    description: '7-limit neutral third generator. Two generators = 3/2. Excellent no-fives temperament. 41-note MOS for full power.' },
+    description: '7-limit neutral third generator. Two generators = 3/2. Excellent no-fives temperament. This is the "accurate" neutral third temperament, where stacking a third gives you a pythagorean fifth. Imagine the diatonic scale with pure fifths, and then just clone it and offset the clone by ~350c.' },
 
   { name: 'Suhajira', mos: ['3L_4s', '7L_3s', '10L_7s'], genRange: [352.94, 365.0],
     commas: ['243/242', '2401/2400'], commaNames: ['rastma', 'breedsma'],
     wikiUrl: 'https://en.xen.wiki/w/Rastmic_clan#Suhajira',
-    description: 'Rastmic clan. <a href="https://en.xen.wiki/w/Dicot_family" target="_blank" style="color:#c49bd4;text-decoration:none">Dicot</a> tuning, e.g. this divides the fifth into 2 parts. Sharp neutral third generator. Two generators = 3/2.' },
+    description: 'Rastmic clan. <a href="https://en.xen.wiki/w/Dicot_family" target="_blank" style="color:#c49bd4;text-decoration:none">Dicot</a> tuning, e.g. this divides the fifth into 2 parts. This is the "sharp" neutral third temperament, which when stacked, gives you superpyth fifths. Evil twin of mohajira.' },
 
   { name: 'Magic', mos: ['3L_4s', '3L_7s', '3L_10s'], genRange: [377.0, 385.0],
     commas: ['3125/3072'], commaNames: ['magic comma'],
@@ -112,7 +112,7 @@ const TEMPERAMENTS = [
   { name: 'Rarity', mos: ['4L_3s', '4L_7s'], genRange: [327.27, 333.33],
     commas: ['1035/1024', '16875/16606', '192375/188416'], commaNames: ['rarity comma', '16875/16606', '192375/188416'],
     wikiUrl: 'https://en.xen.wiki/w/No-sevens_subgroup_temperaments#Rarity',
-    description: '2.3.5.19.23 subgroup. Generator is ~368/225. Offers a 7L 4s scale of reasonable hardness in the 5-limit among small edos.' },
+    description: '2.3.5.19.23 subgroup. Generator is ~368/225. Offers a 7L 4s scale of reasonable hardness in the 5-limit among small edos. <br> <br> This temperament is especially unique and interesting in that theres <i> nothing good here at all </i>. In a world of crazy temperaments, just intervals and cluster chords, Rarity is a complete standout in that it just... fails to do what everything else does.' },
 
   // === PENTATONIC (2L 3s) ===
 
@@ -130,7 +130,7 @@ const TEMPERAMENTS = [
   { name: 'Baldi', mos: ['1L_4s', '5L_1s', '6L_5s'], genRange: [200.0, 208.70],
     commas: ['225/224', '325/324', '640/637'], commaNames: ['septimal kleisma', 'lesser diesis', '640/637'],
     wikiUrl: 'https://en.xen.wiki/w/Subgroup_temperaments#Baldy',
-    description: '2.9.5.7.13 subgroup. Derived from every other generator of garibaldi. Generator ~200–207¢. You can also just think of this as every other generator from pythagorean - so its a wholetone scale temperament. Just a big stack of wholetones.' },
+    description: '2.9.5.7.13 subgroup. Derived from every other generator of garibaldi. Generator ~200–207¢. <br> <br> You can also just think of this as every other generator from pythagorean - so its a wholetone scale temperament. Just a biiig stack of wholetones, but with a weird comma-pump thing goin instead of wrapping back around like 6edo does.' },
 
   { name: 'Machine', mos: ['1L_4s', '5L_1s', '6L_5s'], genRange: [208.7, 218.18],
     commas: ['16807/16384'], commaNames: ['cloudy comma'],
@@ -140,7 +140,7 @@ const TEMPERAMENTS = [
   { name: 'Kumonga', mos: ['1L_4s', '5L_1s', '5L_6s'], genRange: [218.18, 222.22],
     commas: ['126/125'], commaNames: ['starling comma'],
     wikiUrl: 'https://en.xen.wiki/w/Starling_temperaments#Kumonga',
-    description: 'Starling family. Generator is a sharp 8/7. Tempers out 126/125.' },
+    description: 'Starling family. Generator is a sharp 8/7. Tempers out 126/125. <br> <br> Gonna be honest, theres nothing that interesting in this interval region. You cannot imagine how many strings I had to pull to find out what temperament could go here, the amount of research I had to do - and the result? Kumonga. Ok. <br> <br> Maybe you can make something work here. Prove these lousy tuning theorists wrong. I believe in you. ' },
 
   { name: 'Hemisensi', mos: ['1L_4s', '5L_1s', '5L_6s'], genRange: [222.22, 228.57],
     commas: ['78732/78125'], commaNames: ['sensipent comma'],
@@ -150,38 +150,38 @@ const TEMPERAMENTS = [
   { name: 'Tricot / Slendric', mos: ['1L_4s', '5L_1s', '5L_6s'], genRange: [228.57, 240.0],
     commas: ['1029/1024'], commaNames: ['gamelisma'],
     wikiUrl: 'https://en.xen.wiki/w/Slendric',
-    description: 'Three 8/7 generators = 3/2. 2.3.7 subgroup. Very accurate.' },
+    description: 'Three 8/7 generators = 3/2. 2.3.7 subgroup. Very accurate. <br> <br> Historically this temperament has been called slendric, because in the Indonesian Gamelan scale Slendro, some might say their fifth is ~roughly~ divided into 3 steps - even though the step sizes aren&#39t evenly spaced at all. It&#39s a bit chauvinistic if you think about it. Imagine if you will, an alien species coming to earth and they analyze all of western classical music theory. They then fly away and tell the rest of the universe that the earthlings play music in 7edo, and name 7edo "Diatonic". <br> <br> So just call it tricot, will you?' },
 
   { name: 'Glacier / Pentacot', mos: ['1L_7s', '8L_1s'], genRange: [137.0, 144.0],
     commas: [], commaNames: [],
     wikiUrl: 'https://en.xen.wiki/w/Glacier',
-    description: 'Pentacot tuning (divides 3/2 into 5 steps). Generator ~137–144¢.' },
+    description: 'Pentacot tuning (divides 3/2 into 5 steps). Generator ~137–144¢. <br> <br> Honestly, do me a favor and just call this pentacot.' },
 
   { name: 'Bohpier', mos: ['1L_7s', '8L_1s', '8L_9s'], genRange: [144, 146.94],
     commas: ['245/243', '3125/3087'], commaNames: ['sensamagic comma', 'gariboh comma'],
     wikiUrl: 'https://en.xen.wiki/w/Bohpier',
-    description: 'Related to Bohlen-Pierce with octave period. 13 generators = 3rd harmonic, 19 = 5th, 23 = 7th.' },
+    description: 'Related to Bohlen-Pierce with octave period. 13 generators = 3rd harmonic, 19 = 5th, 23 = 7th. <br> <br> Basically someone took the bohlen pierce scale, where you divide 3/1 into 13 even steps, and fudged it up so that it fits into 12edo. Fun stuff.' },
 
 
   { name: 'Tritonic', mos: ['2L_1s', '2L_3s', '2L_5s'], genRange: [617.0, 624.0],
     commas: ['225/224'], commaNames: ['marvel comma'],
     wikiUrl: 'https://en.xen.wiki/w/Marvel_temperaments#Tritonic',
-    description: 'Marvel family. Generator is a 10/7 tritone (~617.5¢). 5 generators = 6th harmonic.' },
+    description: 'Marvel family. Generator is a 10/7 tritone (~617.5¢). 5 generators = 6th harmonic. Pretty much at the absolute limit of listenable music. <br> <br> <i>&#39;Oops! All tritones!&#39;</i>' },
 
   { name: 'Alpha Tricot', mos: ['2L_1s', '2L_3s', '2L_5s', '2L_7s'], genRange: [628.5, 640.0],
     commas: [], commaNames: [],
     wikiUrl: 'https://en.xen.wiki/w/Alphatricot_family#Alphatricot',
-    description: 'Alpha ploidacot family. Generator ~628.5–640¢.' },
+    description: 'Alpha ploidacot family. Generator ~628.5–640¢. <br> <br> This temperament used to just be called tricot. However it behaved completely differently from all over n-cot tunings, and instead of dividing 3/2 into 3, it divided 3/1 into 3.   <br> Which is dumb and I hate it. <br> So after endless complaining and petitioning on forums and wiki spaces, Tricot was renamed to Alpha Tricot (Alpha n-cot divides 3/1 into n), and Slendro is now correctly being named tricot.' },
 
   { name: 'A-team', mos: ['3L_2s', '5L_3s'], genRange: [461.5, 471.4],
     commas: ['225/224', '875/864'], commaNames: ['marvel comma', 'keema'],
     wikiUrl: 'https://en.xen.wiki/w/Subgroup_temperaments#A-team',
-    description: '2.3.5.7 subgroup. Generator between 5\\13 and 11\\28. Tempers out 225/224 and 875/864. Prototypical oneirotonic (5L 3s) temperament. Works well for gamelan scales.' },
+    description: '2.3.5.7 subgroup. Generator between 5\\13 and 11\\28. Tempers out 225/224 and 875/864. Prototypical oneirotonic (5L 3s) temperament. Works well for gamelan scales. <br> <br> Pretty dumb name - pretty cool sounds.' },
 
   { name: 'Buzzard', mos: ['3L_2s', '5L_3s', '5L_8s'], genRange: [471.4, 480.0],
     commas: [], commaNames: [],
     wikiUrl: 'https://en.xen.wiki/w/Buzzard',
-    description: 'Alpha tetracot (splits 3/1 into 4 steps). Given 3/1 is split into 4 steps, you probably want more than 4 steps in your scale! <br> <br> Generator ~471.4–480¢. Prototypical hard oneirotonic(5L 3s) tuning. Works well for gamelan scales.' },
+    description: 'Alpha tetracot (splits 3/1 into 4 steps). Given 3/1 is split into 4 steps, if you put more than 4 steps in your scale you&#39ll get a fifth! Funny how that works out. <br> <br> Generator ~471.4–480¢. Prototypical hard oneirotonic(5L 3s) tuning. Works well for gamelan scales.' },
 
 
   { name: 'Kleismic', mos: ['4L_3s', '4L_7s', '4L_11s'], genRange: [313.0, 320.0],
@@ -245,7 +245,7 @@ const TEMPERAMENTS = [
   { name: 'Tetracot', mos: ['6L_1s', '7L_6s', '7L_13s'], genRange: [171.43, 180.0],
     commas: ['20000/19683'], commaNames: ['tetracot comma'],
     wikiUrl: 'https://en.xen.wiki/w/Tetracot_family',
-    description: 'Four generators = 3/2. Generator is a sub-major second (~176¢). EDOs: 27, 34, 41.' },
+    description: 'Four generators = 3/2. Generator is a sub-major second (~176¢). EDOs: 27, 34, 41. <br> <br> The OG n-cot tuning. The name sounded so cool that we renamed a bunch of other temperaments in its wake. Sort of. To varying degrees of success.' },
 
   { name: 'Didacus', mos: ['6L_1s', '7L_6s', '13L_7s'], genRange: [180, 200.0],
     commas: ['3136/3125'], commaNames: ['hemimean comma'],
@@ -255,27 +255,27 @@ const TEMPERAMENTS = [
   { name: 'Emka', mos: ['2L_9s', '2L_11s', '13L_2s'], genRange: [550.0, 553.8],
     commas: ['3136/3125', '5120/5103'], commaNames: ['hemimean comma', 'hemimean'],
     wikiUrl: 'https://en.xen.wiki/w/Hemimean_clan#Emka',
-    description: 'Hemimean clan. Generator is near 11/8. 11 and 13 note MOS scales.' },
+    description: 'Hemimean clan. Generator is near 11/8. 11 and 13 note MOS scales. <br> <br> Can make some pretty gnarly 11 and 13 limit sounds. Check out the 2L 7s scale if you want to scrunch some faces..' },
 
   { name: 'Octacot', mos: ['1L_13s', '14L_1s', '14L_13s'], genRange: [85.7, 90.0],
     commas: ['20000/19683'], commaNames: ['tetracot comma'],
     wikiUrl: 'https://en.xen.wiki/w/Tetracot_family#Octacot',
     description: 'Tetracot family. Eight generators = 3/2. Half of a tetracot generator.' },
 
-  { name: 'Passion', mos: ['1L_11s', '12L_1s', '12L_13s'], genRange: [96.0, 103.0],
+  { name: 'Passion', mos: ['1L_11s', '12L_1s', '12L_13s'], genRange: [96.0, 100.0],
     commas: ['262144/253125'], commaNames: ['passion comma'],
     wikiUrl: 'https://en.xen.wiki/w/Passion_family#Passion',
-    description: 'Near-12edo generator (~98¢). Twelve generators = octave minus a comma. Chromatic 13-note MOS.' },
+    description: 'Near-12edo generator (~98¢). Twelve generators = octave minus a comma.... This would probably suck as a scale or temperament. Just use 12edo???.' },
 
   { name: 'Escape', mos: ['1L_20s', '21L_1s', '21L_22s'], genRange: [55.33, 57.2],
     commas: ['4294967296/4271484375'], commaNames: ['escapade comma'],
     wikiUrl: 'https://en.xen.wiki/w/Escapade_family#2.3.5.11_subgroup',
-    description: 'Escapade family. Twenty-one generators = octave. Very small generator ~56¢.' },
+    description: 'Escapade family. Twenty-one generators = octave. Very small generator ~56¢. You can probably get some cool somewhat-detempered 21edo structures out of this.' },
 
   { name: 'Valentine', mos: ['1L_14s', '15L_1s', '15L_16s'], genRange: [75.0, 80.0],
     commas: ['6144/6125'], commaNames: ['porwell comma'],
     wikiUrl: 'https://en.xen.wiki/w/Valentine',
-    description: 'Generator is ~77.1¢. Fifteen generators = 3/2. Excellent 11-limit accuracy.' },
+    description: 'Generator is ~77.1¢. Fifteen generators = 3/2. Excellent 11-limit accuracy. <br> <br> Probably too many generators to reach a fifth if you ask me but what can ya say.' },
 
   // === TRIVIAL / SMALL ===
  // { name: 'Bug', mos: ['4L_1s', '4L_5s'], genRange: [240.0, 264.0],
